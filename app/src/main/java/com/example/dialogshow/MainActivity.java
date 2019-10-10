1979cb80925d88e952938d671bcb36b9b30348c4
pikaChin <derrfgh4563@gmail.com>
2019-10-11 01:08:10 +0800

    first alert dialog

--- app/src/main/java/com/example/dialogshow/MainActivity.java
+++ app/src/main/java/com/example/dialogshow/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dialogshow;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this).setMessage("Alert Dialog practice!!")
+                                 .setTitle("Welcome")
+                                 .setIcon(R.mipmap.ic_launcher)
+                                 .show();
     }
 }
